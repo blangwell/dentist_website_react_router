953e259458781bd307f6ae21604ffabb0e3133df
blangwell <b.langwell@outlook.com>
2020-08-28 10:52:33 -0700

    [home] Add Home component

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Home extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Barent Dentistry</h1>
+                <p>Where the Dentists go for their Dental Needs</p>
+            </div>
+        )
+    }
+}
+
+export default Home;
