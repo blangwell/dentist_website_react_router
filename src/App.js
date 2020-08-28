f77366b1f923d0fbd3b0bf990c7c934f834008c3
blangwell <b.langwell@outlook.com>
2020-08-28 11:10:22 -0700

    [app] Add routes to component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
 import Home from './Home';
 import Procedures from './Procedures';
 import Contact from './Contact';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <Procedures />
-      <Contact />
-    </div>
+    <Router>
+      <div className="App">
+      {/* each of these components need to have its own route */}
+        {/* use the exact keyword so that every page with a / in the url wont show home component */}
+        <Route exact path='/' component={Home} />
+        <Route path='/procedures' component={Procedures} />
+        <Route path='/contact' component={Contact} />
+      </div>
+    </Router>
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
 
 class Home extends Component {
-    render() {
-        return(
-            <div>
-                <h1>Barent Dentistry</h1>
-                <p>Where the Dentists go for their Dental Needs</p>
-            </div>
-        )
-    }
+  render() {
+    return(
+      <div>
+					<h1>Barent Dentistry</h1>
+					<p>Where the Dentists go for their Dental Needs</p>
+			</div>
+		)
+	}
 }
 
 export default Home;
