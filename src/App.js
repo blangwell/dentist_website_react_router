59d8c75c9bdc908c4b60c1b154eeb1c9ad9f292d
blangwell <b.langwell@outlook.com>
2020-08-28 11:30:09 -0700

    [procedures] Add procedures as props

--- src/App.js
+++ src/App.js
@@ -12,18 +12,26 @@ import Procedures from './Procedures';
 import Contact from './Contact';
 
 function App() {
+  const procedures = [
+    'deep cleaning',
+    'nitrous oxide infusions',
+    'root canals',
+    'crowns',
+    'implants'
+  ]
   return (
     <Router>
       <div className="App">
-      <nav>
-        <Link to='/'>Home</Link>{' '}
-        <Link to='/procedures'>Procedures </Link>{' '}
-        <Link to='/contact'>Contact us</Link>{' '}
-      </nav>
+        <nav>
+        {/* React removes whitespace when it compiles, hence {' '} */}
+          <Link to='/'>Home</Link>{' '}
+          <Link to='/procedures'>Procedures </Link>{' '}
+          <Link to='/contact'>Contact us</Link>{' '}
+        </nav>
       {/* each of these components need to have its own route */}
         {/* use the exact keyword so that every page with a / in the url wont show home component */}
         <Route exact path='/' component={Home} />
-        <Route path='/procedures' component={Procedures} />
+        <Route path='/procedures' render={ ()=> <Procedures procedures={procedures}/> } />
         <Route path='/contact' component={Contact} />
       </div>
     </Router>
--- src/Procedures.js
+++ src/Procedures.js
@@ -1,13 +1,18 @@
 import React, {Component} from 'react';
 
 class Procedures extends Component {
-    render(){
-        return(
-            <div>
-                <h1>Procedures</h1>
-            </div>
-        )
-    }
+  render(){
+    const proceduresList = this.props.procedures.map((p, idx) => {
+      console.log(idx)
+      return <li key={idx}>{p}</li>
+    })
+    return(
+      <div>
+        <h1>Procedures</h1>
+        {proceduresList}
+      </div>
+    )
+  }
 }
 
 export default Procedures;
