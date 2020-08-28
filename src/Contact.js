964033f631f64fd936061017e1be26de7b620a9d
blangwell <b.langwell@outlook.com>
2020-08-28 10:59:18 -0700

    [contact, procedures] Add Contact and Procedures components

---
+++ src/Contact.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Contact extends Component {
+    render(){
+        return(
+            <div>
+                <h1>Contact</h1>
+                <p>Barent Langwell DDS</p>
+                <p>Email: butt-stuff@butts.org</p>
+                <p>420-420-6969</p>
+            </div>
+        )
+    }
+}
+
+export default Contact;
